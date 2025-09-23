567c3eb695d99ee3204ae015a074bb3ccbb7f3ee
rodrigossbjj <rodrigosousasales9@gmail.com>
2025-09-23 19:14:24 -0300

    feat: dockerizar FastAPI com PostgreSQL e verificar containers funcionando

--- app/core/env.py
+++ app/core/env.py
@@ -1,11 +1,12 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_JWT: str
 
     class Config:
-        env_file = ".env"
+        env_file = os.getenv("ENV_FILE", ".env")
         env_file_encoding = "utf-8"
 
 settings = Settings()
